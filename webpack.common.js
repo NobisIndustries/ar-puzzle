ed0f555e8353019a5213006bce9ebe94a68b967d
Fabian <fabiannobis@gmx.de>
2022-03-29 18:01:54 +0200

    Prod config

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './src/code.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000,
-
     },
     module: {
         rules: [
